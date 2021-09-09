25e1e0c18871596f9407782fee9905acc63482da
bryahchin.mihail <bryahchin.mihail@otr.ru>
2021-09-09 14:36:55 +0300

    Initial commit

---
+++ src/test/java/ru/otus/dz/OtusBootExpApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.dz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusBootExpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
